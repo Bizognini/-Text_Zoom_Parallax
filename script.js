800080d718444f7a2d3560148e0cbcd51b35e964
Enzo <enzobisneto@gmail.com>
2026-04-22 14:55:54 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,43 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let valorMaskSize = "2700vw"
+
+if(window.innerWidth < 1000){
+    valorMaskSize = "4000vw"
+}
+
+gsap.to(".mask", {
+    maskSize: valorMaskSize,
+    maskPosition: "53% center",
+    scrollTrigger: {
+        trigger: ".container",
+        pin: true,
+        start: "top top",
+        end: "bottom 30%",
+        scrub: 2
+    }
+})
+
+const tl = gsap.timeline({defaults:{duration: .5}});
+
+
+tl.from(".txtEfeito1", {
+    y: 40,
+    opacity: 0,
+    filter: "blur(20px)",
+    scrollTrigger: {
+        trigger: ".txtEfeito1",
+        start: "80% 50%",
+        markers: true,
+        scrub: 2,
+    }
+}, "+=1").from(".txtEfeito2", {
+    y:40,
+    opacity: 0,
+    filter: "blur(20px)",
+    scrollTrigger: {
+        trigger: ".txtEfeito2",
+        start: "80% 50%",
+        scrub: 2,
+    }
+}, "+=.1")
